99169e3479034111c116be482f093530ef0dc926
Sumit5151 <sumitshitole5151@gmail.com>
2023-03-26 12:04:36 +0530

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SecondASPDotNetCoreMVCWebApp.Models;
+
+namespace SecondASPDotNetCoreMVCWebApp.Controllers
+{
+    public class UserController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            SecondMvcappDbContext db = new SecondMvcappDbContext();
+            var users = db.Users.ToList();
+
+
+            return View(users);
+
+        }
+    }
+}
